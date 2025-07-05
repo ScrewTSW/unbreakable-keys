ff94e1afe5051da67b115d392e09ec544cc35d12
Tibor Dancs <tdancs@redhat.com>
2025-07-05 13:43:59 +0200

    Updated mod source in accordance to https://github.com/sp-tarkov/mod-examples
    Signed-off-by: Tibor Dancs <tdancs@redhat.com>

--- src/mod.ts
+++ src/mod.ts
@@ -5,7 +5,7 @@ import { ILogger } from "@spt/models/spt/utils/ILogger";
 import { DatabaseServer } from "@spt/servers/DatabaseServer";
 import { IDatabaseTables } from "@spt/models/spt/server/IDatabaseTables";
 
-class UbreakableKeys implements IPostDBLoadMod 
+class UbreakableKeys implements IPostDBLoadMod
 {
 
     private modConfig = require("../config/config.json");
@@ -14,7 +14,7 @@ class UbreakableKeys implements IPostDBLoadMod
     private color = require("C:/snapshot/project/obj/models/spt/logging/LogTextColor");
     private baseClasses = require("C:/snapshot/project/obj/models/enums/BaseClasses");
 
-    public postDBLoad(container: DependencyContainer): void 
+    public postDBLoad(container: DependencyContainer): void
     {
 
         const config = this.modConfig
@@ -25,37 +25,37 @@ class UbreakableKeys implements IPostDBLoadMod
         const databaseServer = container.resolve<DatabaseServer>("DatabaseServer");
         const tables: IDatabaseTables = databaseServer.getTables();
         const items = Object.values(tables.templates.items);
-    
+
         // get logger
         const logger = container.resolve<ILogger>("WinstonLogger");
 
         // Logic
-        for (const item in items) 
+        for (const item in items)
         {
             const itemProps = items[item]._props;
 
             if (config.enable_blacklist && config.blacklisted_keys.includes(items[item]._id))
                 continue;
 
-            if (items[item]._parent == baseClasses.KEY_MECHANICAL) 
+            if (items[item]._parent == baseClasses.KEY_MECHANICAL)
             {
                 itemProps.DiscardLimit = -1;
 
-                if (config.unbreakable_keys) 
+                if (config.unbreakable_keys)
                     itemProps.MaximumNumberOfUsage = 0;
 
-                if (config.weightless_keys) 
+                if (config.weightless_keys)
                     itemProps.Weight = 0.0;
             }
 
-            if (items[item]._parent == baseClasses.KEYCARD) 
+            if (items[item]._parent == baseClasses.KEYCARD)
             {
                 itemProps.DiscardLimit = -1;
 
-                if (config.unbreakable_keycards) 
+                if (config.unbreakable_keycards)
                     itemProps.MaximumNumberOfUsage = 0;
 
-                if (config.weightless_keycards) 
+                if (config.weightless_keycards)
                     itemProps.Weight = 0.0;
             }
         }
